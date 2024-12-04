186e916e78d2a839abb1c46bb942426e7890ba23
David Losert <davelosert@github.com>
2024-12-04 07:01:05 +0000

    test: Extends mocktokit to specifiy paginate url and adds tests for deploymentStatus

--- src/DemoDeploymentReview.test.ts
+++ src/DemoDeploymentReview.test.ts
@@ -1,15 +1,14 @@
 import { describe, expect, it } from 'vitest';
 import { createMockDeployment } from '../test/moctokit/DeploymentMockFactory.js';
+import { createMockDeploymentStatus } from '../test/moctokit/DeploymentStatusMockFactory.js';
 import { createMocktokit } from '../test/moctokit/Mocktokit.js';
 import { DemoDeploymentReview } from './DemoDeploymentReview.js';
-
-
 
 describe('DemoDeploymentReview', () => {
   describe("createDemoReview()", async () => {
     it('throws if returned deployment has different task than "demo:deployment"', async () => {
       const moctokit = createMocktokit();
-      moctokit.paginate.mockResolvedValueOnce( [createMockDeployment({ task: 'not-a-demo' })]);
+      moctokit.paginateCalledWith("GET /repos/{owner}/{repo}/deployments").mockResolvedValueOnce( [createMockDeployment({ task: 'not-a-demo' })]);
 
       await expect(DemoDeploymentReview.createDemoReview(moctokit, { owner: 'octodemo', repo: 'bootstrap' })).rejects.toThrow("Invalid payload type not-a-demo");
     });
@@ -29,6 +28,50 @@ describe('DemoDeploymentReview', () => {
       expect(results).toHaveLength(2);
     });
 
+    it('loads all deployments in the constructors and caches the results after.', async () => {
+      const moctokit = createMocktokit();
+      moctokit.paginate.mockResolvedValueOnce( [createMockDeployment(), createMockDeployment()]);
+
+      const demoDeploymentReview = await DemoDeploymentReview.createDemoReview(moctokit, { owner: 'octodemo', repo: 'bootstrap' });
+
+      expect(moctokit.paginate).toBeCalledTimes(1);
+      await demoDeploymentReview.getAllDemoDeployments();
+      expect(moctokit.paginate).toBeCalledTimes(1);
+    });
+  });
+
+  describe("analyze()", async () => {
+    it('returns empty results if no deployments are returned', async () => {
+      const moctokit = createMocktokit();
+      moctokit.paginate.mockResolvedValueOnce( []);
+
+      const demoDeploymentReview = await DemoDeploymentReview.createDemoReview(moctokit, { owner: 'octodemo', repo: 'bootstrap' });
+
+      const results = await demoDeploymentReview.analyze();
+
+      expect(results.errored).toHaveLength(0);
+      expect(results.on_hold).toHaveLength(0);
+      expect(results.processed).toHaveLength(0);
+      expect(results.to_terminate).toHaveLength(0);
+    });
+
+    it('correctly identifies demos on hold', async () => {
+      const moctokit = createMocktokit();
+      const demoDeployment = createMockDeployment({ task: 'demo:deployment' });
+
+      moctokit.paginateCalledWith("GET /repos/{owner}/{repo}/deployments").mockResolvedValueOnce([demoDeployment]);
+
+      moctokit.rest.repos.getDeployment.mockResolvedValueOnce({ data: demoDeployment, headers: {}, status: 200, url: '' });
+
+      moctokit.paginateCalledWith("GET /repos/{owner}/{repo}/deployments/{deployment_id}/statuses").mockResolvedValue([createMockDeploymentStatus({ description: 'demo::lifecycle_hold' })]);
+
+      const demoDeploymentReview = await DemoDeploymentReview.createDemoReview(moctokit, { owner: 'octodemo', repo: 'bootstrap' });
+
+      const results = await demoDeploymentReview.analyze();
+
+      expect(results.on_hold).toHaveLength(1);
+      expect(results.on_hold[0].demo.id).toBe(demoDeployment.id);
+    });
   });
 
 });
--- test/moctokit/Mocktokit.ts
+++ test/moctokit/Mocktokit.ts
@@ -1,10 +1,25 @@
+import { PaginateInterface } from "@octokit/plugin-paginate-rest";
 import { Octokit } from '@octokit/rest';
-import { DeepMockProxy, mockDeep } from 'vitest-mock-extended';
+import { CalledWithMock, DeepMockProxy, mockDeep } from 'vitest-mock-extended';
 
-function createMocktokit(): DeepMockProxy<Octokit> {
-  const mockObj: DeepMockProxy<Octokit> = mockDeep<Octokit>();
+type PaginateArgs = Parameters<PaginateInterface>[0];
 
-  return mockObj;
+type CalledWith = ReturnType<CalledWithMock<any, any>>;
+
+type Mocktokit = DeepMockProxy<Octokit> & {
+  paginateCalledWith(path: string): CalledWith;
+}
+
+
+function createMocktokit(): Mocktokit {
+  const octokit: DeepMockProxy<Octokit> = mockDeep<Octokit>();
+
+
+  function paginateCalledWith(path: string): CalledWith {
+    return octokit.paginate.calledWith(path as unknown as PaginateArgs);
+  }
+
+  return Object.assign(octokit, { paginateCalledWith });
 }
 
 export {
